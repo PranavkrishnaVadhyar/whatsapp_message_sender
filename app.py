4a189aa401756fa1417de983c4def0278d8e07cf
B Pranavkrishna Vadhyar <96326149+PranavkrishnaVadhyar@users.noreply.github.com>
2022-12-17 19:30:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, request, redirect,url_for
-from datetime import datetime
+from flask import Flask, render_template, request, redirect,url_for,flash
+from datetime import datetime, timedelta
 import pywhatkit
+import os
+from werkzeug.utils import secure_filename
+from os.path import join, dirname, realpath
+import pandas as pd
 from threading import Timer
 
 
 app = Flask (__name__)
+
+UPLOAD_FOLDER = '/static/files'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app_root = os.path.dirname(os.path.abspath(__file__))
+
 
 
 @app.route("/")
@@ -16,11 +25,38 @@ def index():
 def getnew_form():
     
     msg = request.form.get("message")
-    phone = request.form.get("number")
-    
-    pywhatkit.sendwhatmsg (phone,msg,13,24,15,True,2)
-    
+    phone = str(request.form.get("number"))
+    hour = datetime.now().hour
+    minute = datetime.now().minute 
+    pywhatkit.sendwhatmsg ( "+91"+phone,msg,18,59,15,True,2)
+    flash('The message has been sent')
     return redirect(url_for("/sms"))
+
+@app.route("/file", methods=['POST','GET'])
+
+def upload_file():
+    target = os.path.join(app_root, UPLOAD_FOLDER)
+    msg = request.form.get("message") 
+    if not os.path.isdir(target):
+        os.makedirs(target)
+    if request.method == 'POST':
+        file = request.files['file']
+        file_name = file.filename or ''
+        destination = '/'.join([target, file_name])
+        file.save(destination)
+        # CVS Column Names
+        col_name = 'phonenumber'
+        msg = str(request.form.get("number"))
+        hour = datetime.now().hour
+        minute = datetime.now().minute 
+        # Use Pandas to parse the CSV file
+        csvData = pd.read_csv(destination,names=col_name, header=None)
+        # Loop through the Rows
+        for i in csvData.iterrows():
+            pywhatkit.sendwhatmsg ( "+91"+i,msg,hour,minute,15,True,2)
+    flash('The message has been sent')
+    return redirect(url_for('/file'))
+      
 
 
 if __name__ == '__main__':
