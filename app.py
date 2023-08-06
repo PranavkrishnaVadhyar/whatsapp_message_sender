cfa048b6b83fa315ff7c0ab84cfffef08a74f045
B Pranavkrishna Vadhyar <96326149+PranavkrishnaVadhyar@users.noreply.github.com>
2023-08-06 16:19:32 +0530

    Update app.py 1
    Optimized the code

--- app.py
+++ app.py
@@ -1,40 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
-from datetime import datetime, timedelta
+from datetime import datetime
 import pywhatkit
 import os
-from werkzeug.utils import secure_filename
-from os.path import join, dirname, realpath
 import pandas as pd
-from threading import Timer
-
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "/static/files"
+UPLOAD_FOLDER = "static/files"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app_root = os.path.dirname(os.path.abspath(__file__))
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-
-@app.route("/sms", methods=["POST", "GET"])
-def getnew_form():
-
+@app.route("/sms", methods=["POST"])
+def send_sms():
     msg = request.form.get("message")
-    phone = str(request.form.get("number"))
+    phone = "+91" + str(request.form.get("number"))
     hour = datetime.now().hour
     minute = datetime.now().minute
-    pywhatkit.sendwhatmsg("+91" + phone, msg, 18, 59, 15, True, 2)
+    pywhatkit.sendwhatmsg(phone, msg, hour, minute, 15, True, 2)
     flash("The message has been sent")
-    return redirect(url_for("/sms"))
+    return redirect(url_for("index"))
 
-
-@app.route("/file", methods=["POST", "GET"])
+@app.route("/file", methods=["POST"])
 def upload_file():
-    target = os.path.join(app_root, UPLOAD_FOLDER)
+    target = os.path.join(app_root, app.config["UPLOAD_FOLDER"])
     msg = request.form.get("message")
     
     if not os.path.isdir(target):
@@ -42,25 +34,25 @@ def upload_file():
         
     if request.method == "POST":
         file = request.files["file"]
-        file_name = file.filename or ""
-        destination = "/".join([target, file_name])
+        file_name = secure_filename(file.filename)
+        destination = os.path.join(target, file_name)
         file.save(destination)
         
-        # CVS Column Names
         col_name = "phonenumber"
-        msg = str(request.form.get("number"))
+        
+        # Use Pandas to parse the CSV file
+        csvData = pd.read_csv(destination, names=[col_name], header=None)
+        
         hour = datetime.now().hour
         minute = datetime.now().minute
         
-        # Use Pandas to parse the CSV file
-        csvData = pd.read_csv(destination, names=col_name, header=None)
-        
-        # Loop through the Rows
-        for i in csvData.iterrows():
-            pywhatkit.sendwhatmsg("+91" + i, msg, hour, minute, 15, True, 2)
+        # Loop through the rows
+        for i, row in csvData.iterrows():
+            pywhatkit.sendwhatmsg("+91" + str(row[col_name]), msg, hour, minute, 15, True, 2)
+    
     flash("The message has been sent")
-    return redirect(url_for("/file"))
-
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
+    app.secret_key = "supersecretkey"
     app.run(host="0.0.0.0", port=80)
